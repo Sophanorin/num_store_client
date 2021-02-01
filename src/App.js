0c1f65427207b3f58ba06795086b7631dfc3619b
Sophanorin <heousopharin168@gmail.com>
2021-02-01 16:39:54 +0700

    config route in App.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
         <Route path="/register" component={RegisterScreen} />
         <Route path="/checkout" component={CheckoutScreen} />
         <Route path="/products/:id" exact component={ProductDetial} />
-        <Route path="/shop" exact component={ShopScreen} />
+        <Route path="/shop" component={ShopScreen} />
         <Route path="/order/:id" exact component={OrderDetailScreen} />
         <Route path="/ordershistory" component={OrderHistoryScreen} />
         <Route path="/wishlist" component={WishlistScreen} />
@@ -68,6 +68,7 @@ function App() {
           path="/categorylist"
           component={CategoryListScreen}
         ></AdminRoute>
+        <Route path="*" component={UnavailablePage} />
       </Switch>
       <footer id="footer" className="section footer">
         <Footer />
